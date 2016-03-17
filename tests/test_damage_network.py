06f3a38d66bbcf75d6b5ada34cba17038003965d
Sudipta Basak <basaks@gmail.com>
2016-03-17 16:20:25 +1100

    [tests] damaged network test added

--- tests/test_damage_network.py
+++ tests/test_damage_network.py
@@ -13,8 +13,8 @@ class DamagedNetowrkTest(unittest.TestCase):
         from wistl.config_class import TransmissionConfig
         conf_path = os.path.join(WISTL, 'tests', 'test.cfg')
         assert os.path.exists(conf_path), 'config file path wrong'
-        conf = TransmissionConfig(conf_path)
-        self.damaged_networks = create_damaged_network(conf)
+        self.conf = TransmissionConfig(conf_path)
+        self.damaged_networks = create_damaged_network(self.conf)
 
     def test_time_index(self):
         keys = self.damaged_networks.keys()  # get one key
@@ -24,9 +24,16 @@ class DamagedNetowrkTest(unittest.TestCase):
         for k, d_n in self.damaged_networks.iteritems():
             self.assertTrue(d_n0.time_index.equals(d_n.time_index))
 
+    def test_number_of_damaged_networks(self):
+        self.assertEqual(len(self.damaged_networks),
+                         len(self.conf.path_wind_scenario))
 
-
-
+    def test_damaged_network_type(self):
+        from wistl.damage_network import DamageNetwork
+        from wistl.transmission_network import TransmissionNetwork
+        for d in self.damaged_networks.itervalues():
+            self.assertTrue(isinstance(d, DamageNetwork))
+            self.assertTrue(isinstance(d, TransmissionNetwork))
 
 if __name__ == '__main__':
     unittest.main()
